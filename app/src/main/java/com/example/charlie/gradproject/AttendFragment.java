64d3673384abef906933b0809709e845b647b975
charlie <526825550@qq.com>
2018-04-16 17:30:15 +0800

    todo: adapter

--- app/src/main/java/com/example/charlie/gradproject/AttendFragment.java
+++ app/src/main/java/com/example/charlie/gradproject/AttendFragment.java
@@ -6,30 +6,18 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.ViewGroup;;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class AttendFragment extends Fragment {
-	// TODO: Rename parameter arguments, choose names that match
-	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-	private static final String ARG_PARAM1 = "param1";
-	private static final String ARG_PARAM2 = "param2";
-
-	// TODO: Rename and change types of parameters
-	private String mParam1;
-	private String mParam2;
-
-	private OnFragmentInteractionListener mListener;
+public class AttendFragment extends Fragment implements AdapterView.OnItemClickListener {
+	private ListView classAttend;
+	private Adapter adapter;
 
 	public AttendFragment() {
 		// Required empty public constructor
 	}
 
-	/**
-	 * Use this factory method to create a new instance of
-	 * this fragment using the provided parameters.
-	 * @return A new instance of fragment ScheduleFragment.
-	 */
-	// TODO: Rename and change types and number of parameters
 	public static AttendFragment newInstance() {
 		return new AttendFragment();
 	}
@@ -43,16 +31,14 @@ public class AttendFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_attend, container, false);
+		View view= inflater.inflate(R.layout.fragment_attend, container, false);
+		adapter=new Adapter();
+		classAttend=view.findViewById(R.id.class_attend);
+		classAttend.setAdapter(adapter);
+		classAttend.setOnItemClickListener(this);
+		return view;
 	}
 
-	// TODO: Rename method, update argument and hook method into UI event
-	public void onButtonPressed(Uri uri) {
-		if (mListener != null) {
-			mListener.onFragmentInteraction(uri);
-		}
-	}
 
 	@Override
 	public void onAttach(Context context) {
@@ -62,11 +48,10 @@ public class AttendFragment extends Fragment {
 	@Override
 	public void onDetach() {
 		super.onDetach();
-		mListener = null;
 	}
 
-	public interface OnFragmentInteractionListener {
-		// TODO: Update argument type and name
-		void onFragmentInteraction(Uri uri);
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
 	}
 }
